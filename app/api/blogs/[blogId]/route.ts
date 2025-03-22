cb465ffa80c6fde851e33833d5d9d3b7797908a8
Vaibhav Verma <vaibhavverma7475@gmail.com>
2025-03-22 16:08:45 +0530

    fixed typo

--- app/api/blogs/[blogId]/route.ts
+++ app/api/blogs/[blogId]/route.ts
@@ -196,48 +196,47 @@ export async function GET(req: Request, { params }: { params: Promise<{ blogId: 
   }
 }
 
-export async function DELETE(req: Request, { params }: { params: Promise<{ blogId: string }> }) {
-  try {
-    await connectDB();
-    const { blogId } = await params;
-    
-    const session = await getServerSession(authOptions);
-    const user: User = session?.user as User;
-    
-    if (!session || !user) {
-      return NextResponse.json({ error: 'Unauthorized. User must be logged in.' }, { status: 401 });
-    }
-    
-    const userId = new mongoose.Types.ObjectId(user.id);
-    
-    let blog = await BlogEModel.findOne({ _id: blogId, author: userId });
-    let BlogModel = BlogEModel;
-    
-    if (!blog) {
-      blog = await BlogMModel.findOne({ _id: blogId, author: user.id });
-      BlogModel = BlogMModel;
-    }
-    
-    if (!blog) {
-      return NextResponse.json({ error: 'Unauthorized. User cannot delete another user\'s blog post or blog not found.' }, { status: 401 });
-    }
-    
-    const notion = await NotionModel.findOne({ _id: blog.notionId });
-    if (notion) {
-      await NotionModel.deleteOne({ _id: blog.notionId });
-    }
-    
-    const deletedBlog = await BlogModel.findOneAndDelete({ _id: blogId, author: user.id });
-    
-    return NextResponse.json({
-      success: true,
-      message: 'Blog deleted successfully',
-      data: deletedBlog
-    }, { status: 200 });
-  } catch (error) {
-    console.error('Error deleting blog:', error);
-    return NextResponse.json({
-      error: 'Failed to delete blog post',
-    }, { status: 500 });
-  }
-}
+// export async function DELETE(req: Request, { params }: { params: Promise<{ blogId: string }> }) {
+//   try {
+//     await connectDB();
+//     const { blogId } = await params;
+//
+//     const session = await getServerSession(authOptions);
+//     const user: User = session?.user as User;
+//
+//     if (!session || !user) {
+//       return NextResponse.json({ error: 'Unauthorized. User must be logged in.' }, { status: 401 });
+//     }
+//
+//     const userId = new mongoose.Types.ObjectId(user.id);
+//
+//     let blog = await BlogEModel.findOne({ _id: blogId, author: userId });
+//     let BlogModel = BlogEModel;
+//
+//     if (!blog) {
+//       blog = await BlogMModel.findOne({ _id: blogId, author: user.id });
+//     }
+//
+//     if (!blog) {
+//       return NextResponse.json({ error: 'Unauthorized. User cannot delete another user\'s blog post or blog not found.' }, { status: 401 });
+//     }
+//
+//     const notion = await NotionModel.findOne({ _id: blog.notionId });
+//     if (notion) {
+//       await NotionModel.deleteOne({ _id: blog.notionId });
+//     }
+//
+//     const deletedBlog = await BlogModel.findOneAndDelete({ _id: blogId, author: user.id });
+//
+//     return NextResponse.json({
+//       success: true,
+//       message: 'Blog deleted successfully',
+//       data: deletedBlog
+//     }, { status: 200 });
+//   } catch (error) {
+//     console.error('Error deleting blog:', error);
+//     return NextResponse.json({
+//       error: 'Failed to delete blog post',
+//     }, { status: 500 });
+//   }
+// }
